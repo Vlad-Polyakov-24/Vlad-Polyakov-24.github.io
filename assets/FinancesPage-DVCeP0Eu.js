c9344e0735d2ae278bdfe9e4a402185b2f76ada3
Vladyslav Poliakov <vladon24121995@gmail.com>
2024-05-31 20:35:20 +0300

    Add files via upload

---
+++ assets/FinancesPage-DVCeP0Eu.js
@@ -0,0 +1,1 @@
+import{r as e,j as s,c as a}from"./index-CjUSHzmp.js";import{S as n}from"./Sidebar-guTt74uW.js";const r={},i=e.memo(()=>s.jsxs(s.Fragment,{children:[s.jsx(n,{title:"Finances Page Sidebar"}),s.jsx("section",{className:a(r.finances,{},[]),children:s.jsx("h1",{children:"Finances Page"})})]}));export{i as default};
---
+++ assets/InventoryPage-DXEhfRi7.js
@@ -0,0 +1,1 @@
+import{r as s,j as e,c as r}from"./index-CjUSHzmp.js";import{S as t}from"./Sidebar-guTt74uW.js";const a={},i=s.memo(()=>e.jsxs(e.Fragment,{children:[e.jsx(t,{title:"Inventory Page Sidebar"}),e.jsx("section",{className:r(a.inventory,{},[]),children:e.jsx("h1",{children:"Inventory Page"})})]}));export{i as default};
