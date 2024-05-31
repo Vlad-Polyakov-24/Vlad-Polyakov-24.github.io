315622187e04a6079886938452c005952436614e
Vladyslav Poliakov <vladon24121995@gmail.com>
2024-05-31 19:10:31 +0300

    Add files via upload

---
+++ testik/assets/FunnelPage-Dq_-s6Uk.js
@@ -0,0 +1,1 @@
+import{r as s,j as e,c as a}from"./index-Cnim4_zE.js";import{S as n}from"./Sidebar-DrI58PBg.js";const r={},o=s.memo(()=>e.jsxs(e.Fragment,{children:[e.jsx(n,{title:"Funnel Page Sidebar"}),e.jsx("section",{className:a(r.funnel,{},[]),children:e.jsx("h1",{children:"Funnel Page"})})]}));export{o as default};
